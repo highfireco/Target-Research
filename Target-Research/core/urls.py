2016a0a9695b564436b9720db49a3e5aaba0fa44
HeroicToad <103355666+Slippery77@users.noreply.github.com>
2026-01-27 23:02:02 +0700

    Update urls.py

--- Target-Research/core/urls.py
+++ Target-Research/core/urls.py
@@ -20,3 +20,8 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from dashboard.views import dashboard_view
+urlpatterns = [
+    path('', dashboard_view, name='dashboard'),
+]
