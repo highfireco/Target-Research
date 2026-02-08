49881b892a10b72c5a45d400333b353d0adc82e6
HeroicToad <103355666+Slippery77@users.noreply.github.com>
2026-02-08 19:18:57 +0700

    save before disater

--- README.md
+++
@@ -1,2 +0,0 @@
-# cool-project 
-Doing something cool for final
--- Target-Research/dashboard/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def dashboard_view(request):
-    return render(request, 'test.html')
-# Create your views here.
