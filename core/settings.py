60e7ee8e980925431e57d1f785629b61d3877787
HeroicToad <103355666+Slippery77@users.noreply.github.com>
2026-02-16 07:38:29 +0700

    Modify STATIC_ROOT and add WhiteNoise storage
    Update STATIC_ROOT path and configure WhiteNoise for static file storage.

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,8 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 import os # ถ้าด้านบนยังไม่ได้ import os ให้ใส่ด้วย
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ส่วนนี้สำหรับให้ WhiteNoise บีบอัดไฟล์ให้เว็บโหลดเร็วขึ้น
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
